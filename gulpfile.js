6c662cbf2c128cdbbc914ae849c1b5286c257576
Aleksandra Filipiak <aleksandra.filipiak@gmail.com>
2018-07-11 23:38:52 +0200

    changes after autoprefixer koala gulp watcher makes new css file, but it's without autoprefixing and it's not as stylesheet in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
-    return gulp.src('scss/**/*.scss')
+    return gulp.src('./scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
             errLogToConsole: true,
             outputStyle: 'expanded'
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('./scss/css'))
 });
 
 
